25f77d607ac9ee8db27f7886b12cb69514feeb74
Kit Trowbridge <catherine.trowbridge@finova.tech>
2026-09-09 16:56:38 +0100

    feat: header component setup

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-header',
+  styleUrl: './header.component.css',
+  templateUrl: './header.component.html',
+})
+
+export class HeaderComponent {}
